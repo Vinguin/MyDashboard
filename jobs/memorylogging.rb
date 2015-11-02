c6e09560f6a1948d32ca24dbbbc0da85a63e222c
Vinh Ngu <vinh-ngu@hotmail.com>
2015-11-02 18:24:04 +0100

    Did you know done

--- jobs/memorylogging.rb
+++ jobs/memorylogging.rb
@@ -7,7 +7,7 @@ SCHEDULER.every '5s', first_in: '1s' do
 	one_percent_value = Integer(memory_stats[7]).to_f / 100.00
 	current_mem_load = Integer(memory_stats[8]).to_f / one_percent_value
 
-	send_event('abc', {value: "#{'%.2f' % current_mem_load}#{as}", max: 100} )
+	send_event('abc', {value: "#{'%.2f' % current_mem_load}", max: 100} )
 
 #	send_event('memory', {value: Integer(memory_stats[8])})
 #	send_event("memory", {max: Integer(memory_stats[7])})
